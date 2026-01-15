9f148c3213ac458a4513d9e04110fa2820e58c6d
Antho <Antho@LAPTOP-RFH4DBAK>
2026-01-15 14:52:32 +0100

    jour 4 : deconection et amélioration page connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,25 +7,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class LoginController extends AbstractController
-{
+class LoginController extends AbstractController {
+
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function index(AuthenticationUtils $authenticationUtils): Response {
         // récupération éventuelle de l'erreur
         $error = $authenticationUtils->getLastAuthenticationError();
         // récupération éventuelle du dernier nom de login utilisé
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
+                    'last_username' => $lastUsername,
+                    'error' => $error,
         ]);
     }
 
-    #[Route('/logout', name: 'logout')]
-    public function logout(): void
-    {
-        // méthode de déconnexion
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void {
+
     }
 }
