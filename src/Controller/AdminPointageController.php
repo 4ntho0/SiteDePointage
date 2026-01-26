d16246c1106d3810fb1641db851bafc57a3ec4a7
Antho <Antho@LAPTOP-RFH4DBAK>
2026-01-26 13:49:54 +0100

    jour 11 : export des pointages terminé

--- src/Controller/AdminPointageController.php
+++ src/Controller/AdminPointageController.php
@@ -11,38 +11,169 @@ use Doctrine\ORM\EntityManagerInterface;
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-class AdminPointageController extends AbstractController {
-
+class AdminPointageController extends AbstractController
+{
     private PointageRepository $repository;
 
-    public function __construct(PointageRepository $repository) {
+    public function __construct(PointageRepository $repository)
+    {
         $this->repository = $repository;
     }
 
     #[Route('/admin/pointages', name: 'admin.pointage', methods: ['GET'])]
-    public function index(Request $request): Response {
+    public function index(Request $request): Response
+    {
         $sortField = $request->query->get('sort', 'datePointage');
         $sortOrder = $request->query->get('order', 'DESC');
         $userFilter = $request->query->get('user');
         $userFilters = $userFilter ? explode(',', $userFilter) : [];
         $page = max(1, (int) $request->query->get('page', 1));
-        $period = $request->query->get('period', 'week');
 
-        // Dates personnalisées (priorité)
+        [$dateStart, $dateEnd, $period] = $this->getDateRangeFromRequest($request);
+
+        $pointages = $this->repository->findAllOrderByFieldWithLimit(
+            $sortField,
+            $sortOrder,
+            $userFilter,
+            $dateStart,
+            $dateEnd
+        );
+
+        $totalPointages = $this->repository->countFiltered($userFilter, $dateStart, $dateEnd);
+        $users = $this->repository->getAllUsernames();
+
+        return $this->render('admin/admin.pointages.html.twig', [
+            'pointages' => $pointages,
+            'users' => $users,
+            'sortField' => $sortField,
+            'sortOrder' => $sortOrder,
+            'userFilter' => $userFilter,
+            'userFilters' => $userFilters,
+            'page' => $page,
+            'totalPointages' => $totalPointages,
+            'period' => $period,
+            'dateStart' => $dateStart,
+            'dateEnd' => $dateEnd,
+            'dateStartInput' => $request->query->get('date_start'),
+            'dateEndInput' => $request->query->get('date_end'),
+        ]);
+    }
+
+    #[Route('/admin/pointages/suppr/{id}', name: 'admin.pointage.suppr', methods: ['GET'])]
+    public function suppr(int $id, PointageRepository $repository): Response
+    {
+        $pointage = $repository->find($id);
+        if ($pointage) {
+            $repository->remove($pointage);
+        }
+        return $this->redirectToRoute('admin.pointage');
+    }
+
+    #[Route('/admin/pointages/edit', name: 'admin.pointage.edit', methods: ['POST'])]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        $id = $request->request->get('id');
+        $pointage = $this->repository->find($id);
+
+        if (!$pointage) {
+            return $this->redirectToRoute('admin.pointage');
+        }
+
+        $date = $request->request->get('datePointage');
+        $entree = $request->request->get('heureEntree');
+        $debut = $request->request->get('heureDebutPause');
+        $fin = $request->request->get('heureFinPause');
+        $sortie = $request->request->get('heureSortie');
+
+        $entree = $entree ? new \DateTime($entree) : null;
+        $debut = $debut ? new \DateTime($debut) : null;
+        $fin = $fin ? new \DateTime($fin) : null;
+        $sortie = $sortie ? new \DateTime($sortie) : null;
+
+        if (($debut && !$fin) || (!$debut && $fin)) {
+            return $this->redirectToRoute('admin.pointage');
+        }
+
+        if (
+            ($entree && $debut && $entree > $debut) ||
+            ($debut && $fin && $debut > $fin) ||
+            ($fin && $sortie && $fin > $sortie) ||
+            ($entree && $sortie && $entree > $sortie)
+        ) {
+            return $this->redirectToRoute('admin.pointage');
+        }
+
+        $pointage->setDatePointage(new \DateTime($date));
+        $pointage->setHeureEntree($entree);
+        $pointage->setHeureDebutPause($debut);
+        $pointage->setHeureFinPause($fin);
+        $pointage->setHeureSortie($sortie);
+
+        $em->flush();
+
+        return $this->redirectToRoute('admin.pointage');
+    }
+
+    #[Route('/admin/pointages/export/pdf', name: 'admin.pointage.export.pdf')]
+    public function exportPdf(Request $request): Response
+    {
+        $sortField = $request->query->get('sort', 'datePointage');
+        $sortOrder = $request->query->get('order', 'DESC');
+        $userFilter = $request->query->get('user');
+
+        [$dateStart, $dateEnd, $period] = $this->getDateRangeFromRequest($request);
+
+        $pointages = $this->repository->findAllOrderByFieldWithLimit(
+            $sortField,
+            $sortOrder,
+            $userFilter,
+            $dateStart,
+            $dateEnd
+        );
+
+        $html = $this->renderView('admin/_pointages_pdf.html.twig', [
+            'pointages' => $pointages,
+            'userFilter' => $userFilter,
+            'dateStart' => $dateStart,
+            'dateEnd' => $dateEnd
+        ]);
+
+        $options = new Options();
+        $options->set('defaultFont', 'Arial');
+
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        return new Response(
+            $dompdf->output(),
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="pointages_' . date('Y-m-d') . '.pdf"'
+            ]
+        );
+    }
+
+    private function getDateRangeFromRequest(Request $request): array
+    {
+        $period = $request->query->get('period', 'week');
+        $page = max(1, (int) $request->query->get('page', 1));
+
         $dateStartInput = $request->query->get('date_start');
         $dateEndInput = $request->query->get('date_end');
 
         $dateStart = null;
         $dateEnd = null;
+
         $today = new \DateTimeImmutable('today');
 
         if ($dateStartInput && $dateEndInput) {
             // Dates personnalisées
             $dateStart = new \DateTimeImmutable($dateStartInput);
             $dateEnd = new \DateTimeImmutable($dateEndInput);
-            $period = 'custom';
         } else {
-            // Logique par période
             switch ($period) {
                 case 'day':
                     $date = $today->modify('-' . ($page - 1) . ' days');
@@ -72,137 +203,12 @@ class AdminPointageController extends AbstractController {
 
                 case 'global':
                 default:
+                    $dateStart = null;
+                    $dateEnd = null;
                     break;
             }
         }
-        $pointages = $this->repository->findAllOrderByFieldWithLimit(
-                $sortField,
-                $sortOrder,
-                $userFilter,
-                $dateStart,
-                $dateEnd
-        );
 
-        $totalPointages = $this->repository->countFiltered($userFilter, $dateStart, $dateEnd);
-        $users = $this->repository->getAllUsernames();
-
-        return $this->render('admin/admin.pointages.html.twig', [
-                    'pointages' => $pointages,
-                    'users' => $users,
-                    'sortField' => $sortField,
-                    'sortOrder' => $sortOrder,
-                    'userFilter' => $userFilter,
-                    'userFilters' => $userFilters,
-                    'page' => $page,
-                    'totalPointages' => $totalPointages,
-                    'period' => $period,
-                    'dateStart' => $dateStart,
-                    'dateEnd' => $dateEnd,
-                    'dateStartInput' => $dateStartInput,
-                    'dateEndInput' => $dateEndInput,
-        ]);
-    }
-
-    #[Route('/admin/pointages/suppr/{id}', name: 'admin.pointage.suppr', methods: ['GET'])]
-    public function suppr(int $id, PointageRepository $repository): Response {
-        $pointage = $repository->find($id);
-        if ($pointage) {
-            $repository->remove($pointage);
-        }
-        return $this->redirectToRoute('admin.pointage');
-    }
-
-    #[Route('/admin/pointages/edit', name: 'admin.pointage.edit', methods: ['POST'])]
-    public function edit(Request $request, EntityManagerInterface $em): Response {
-        $id = $request->request->get('id');
-        $pointage = $this->repository->find($id);
-
-        if (!$pointage) {
-            return $this->redirectToRoute('admin.pointage');
-        }
-
-        $date = $request->request->get('datePointage');
-        $entree = $request->request->get('heureEntree');
-        $debut = $request->request->get('heureDebutPause');
-        $fin = $request->request->get('heureFinPause');
-        $sortie = $request->request->get('heureSortie');
-
-        $entree = $entree ? new \DateTime($entree) : null;
-        $debut = $debut ? new \DateTime($debut) : null;
-        $fin = $fin ? new \DateTime($fin) : null;
-        $sortie = $sortie ? new \DateTime($sortie) : null;
-
-        // Validations (inchangées)
-        if (($debut && !$fin) || (!$debut && $fin)) {
-            return $this->redirectToRoute('admin.pointage');
-        }
-
-        if (
-                ($entree && $debut && $entree > $debut) ||
-                ($debut && $fin && $debut > $fin) ||
-                ($fin && $sortie && $fin > $sortie) ||
-                ($entree && $sortie && $entree > $sortie)
-        ) {
-            return $this->redirectToRoute('admin.pointage');
-        }
-
-        $pointage->setDatePointage(new \DateTime($date));
-        $pointage->setHeureEntree($entree);
-        $pointage->setHeureDebutPause($debut);
-        $pointage->setHeureFinPause($fin);
-        $pointage->setHeureSortie($sortie);
-
-        $em->flush();
-
-        return $this->redirectToRoute('admin.pointage');
-    }
-
-    #[Route('/admin/pointages/export/pdf', name: 'admin.pointage.export.pdf')]
-    public function exportPdf(Request $request): Response {
-        $sortField = $request->query->get('sort', 'datePointage');
-        $sortOrder = $request->query->get('order', 'DESC');
-        $userFilter = $request->query->get('user');
-        $dateStartInput = $request->query->get('date_start');
-        $dateEndInput = $request->query->get('date_end');
-
-        $dateStart = null;
-        $dateEnd = null;
-
-        if ($dateStartInput && $dateEndInput) {
-            $dateStart = new \DateTimeImmutable($dateStartInput);
-            $dateEnd = new \DateTimeImmutable($dateEndInput);
-        }
-
-        $pointages = $this->repository->findAllOrderByFieldWithLimit(
-                $sortField,
-                $sortOrder,
-                $userFilter,
-                $dateStart,
-                $dateEnd
-        );
-
-        $html = $this->renderView('admin/_pointages_pdf.html.twig', [
-            'pointages' => $pointages,
-            'userFilter' => $userFilter,
-            'dateStart' => $dateStart,
-            'dateEnd' => $dateEnd
-        ]);
-
-        $options = new Options();
-        $options->set('defaultFont', 'Arial');
-
-        $dompdf = new Dompdf($options);
-        $dompdf->loadHtml($html);
-        $dompdf->setPaper('A4', 'portrait');
-        $dompdf->render();
-
-        return new Response(
-                $dompdf->output(),
-                200,
-                [
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'attachment; filename="pointages_' . date('Y-m-d') . '.pdf"'
-                ]
-        );
+        return [$dateStart, $dateEnd, $period];
     }
 }
